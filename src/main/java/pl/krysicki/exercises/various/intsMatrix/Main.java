c14e3906755ede8ce0af8b7674d35c158d3eb09e
tomasz-krysicki <tomasz.krysicki@task.com.pl>
2022-09-09 19:45:31 +0200

    task "intsMatrix" - a few amendments for better clarity

--- src/main/java/pl/krysicki/exercises/various/intsMatrix/Main.java
+++ src/main/java/pl/krysicki/exercises/various/intsMatrix/Main.java
@@ -11,12 +11,16 @@ public class Main {
         StringBuilder intsMatrixBuilder = new StringBuilder();
 
         for (int y = 0; y < 2 * n - 1; y++) {
+
             for (int x = 0; x < 2 * n - 1; x++) {
-                intsMatrixBuilder.append(n - Math.min(Math.min(y, 2 * n - 2 - y), Math.min(x, 2 * n - 2 - x)));
-                intsMatrixBuilder.append(" ");
+
+                intsMatrixBuilder.append(n - Math.min(Math.min(y, 2 * (n - 1) - y), Math.min(x, 2 * (n - 1) - x)));
+                intsMatrixBuilder.append(" ");      // single space divider works nicely with n <= 9; for n > 9 TAB divider ("\t") is recommended
             }
+
             intsMatrixBuilder.append("\n");
         }
+
         return String.valueOf(intsMatrixBuilder);
     }
 }
